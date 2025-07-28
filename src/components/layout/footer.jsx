a8946e63cb040fd56589cdd4788981b50776d3fe
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-07-28 12:39:29 +0000

    oke dibagian foter itu ada logo ig dan wa saya mau anda tambhkan emailny

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -41,6 +41,9 @@ export const Footer = () => (
                     <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 4.315 1.847 6.046l-1.107 4.027 4.13-1.082z" />
                 </svg>
             </Link>
+            <Link href="mailto:info@itfestival2025.com" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
+                <Mail size={20} />
+            </Link>
           </div>
         </div>
 
