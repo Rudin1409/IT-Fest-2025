f5f219ba2f05699f1e4f5acf754ae648ef1a286c
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-08 14:20:00 +0000

    untuk contact nya sesuaikan ya dengan gambar ini akun akun nya

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -25,7 +25,7 @@ export const Footer = () => (
           </p>
           {/* Tautan Media Sosial */}
           <div className="flex space-x-4 mt-6">
-            <Link href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
+            <Link href="https://www.instagram.com/ITFESTPOLSRI" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
                 <Instagram size={20} />
             </Link>
             <Link href="https://whatsapp.com" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
@@ -39,7 +39,7 @@ export const Footer = () => (
                     <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 4.315 1.847 6.046l-1.107 4.027 4.13-1.082z" />
                 </svg>
             </Link>
-            <Link href="mailto:info@itfestival2025.com" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
+            <Link href="mailto:ITFESTIVALPOLSRI2025@GMAIL.COM" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
                 <Mail size={20} />
             </Link>
           </div>
@@ -63,11 +63,14 @@ export const Footer = () => (
           <ul className="space-y-3">
             <li className="flex items-start gap-3">
               <Mail size={18} className="text-primary mt-1" />
-              <span>info@itfestival2025.com</span>
+              <span>ITFESTIVALPOLSRI2025@GMAIL.COM</span>
             </li>
             <li className="flex items-start gap-3">
               <Phone size={18} className="text-primary mt-1" />
-              <span>+62 xxx xxxx xxxx</span>
+              <div>
+                <span>+62895604953816</span><br/>
+                <span>+6285366799782</span>
+              </div>
             </li>
             <li className="flex items-start gap-3">
               <MapPin size={18} className="text-primary mt-1" />
