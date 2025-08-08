077c14d75252c485823a66d3753745d1b2f514f5
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-08 14:25:08 +0000

    itu alamat email nya tulisan nya kecil semua bukan cpslock

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -39,7 +39,7 @@ export const Footer = () => (
                     <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 4.315 1.847 6.046l-1.107 4.027 4.13-1.082z" />
                 </svg>
             </Link>
-            <Link href="mailto:ITFESTIVALPOLSRI2025@GMAIL.COM" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
+            <Link href="mailto:itfestivalpolsri2025@gmail.com" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-primary/80 hover:bg-primary transition-colors flex items-center justify-center text-white">
                 <Mail size={20} />
             </Link>
           </div>
@@ -63,7 +63,7 @@ export const Footer = () => (
           <ul className="space-y-3">
             <li className="flex items-start gap-3">
               <Mail size={18} className="text-primary mt-1" />
-              <span>ITFESTIVALPOLSRI2025@GMAIL.COM</span>
+              <span>itfestivalpolsri2025@gmail.com</span>
             </li>
             <li className="flex items-start gap-3">
               <Phone size={18} className="text-primary mt-1" />
@@ -92,3 +92,4 @@ export const Footer = () => (
       </div>
     </footer>
 );
+
