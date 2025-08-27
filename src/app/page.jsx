05b200ed27a2039198c3fa0ff41318f93b357b03
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-27 08:42:56 +0000

    update logo seponsor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -338,6 +338,8 @@ export default function Home() {
     { src: '/sponsor/logo-primary.png', alt: 'Sponsor Logo', hint: 'company logo' },
     { src: '/sponsor/LOGO AP STUDIO.png', alt: 'AP Studio Logo', hint: 'company logo' },
     { src: '/sponsor/LOGO SEWA KAMERA.png', alt: 'Sewa Kamera Logo', hint: 'company logo' },
+    { src: '/sponsor/kiraflorist.PNG', alt: 'Kira Florist Logo', hint: 'company logo' },
+    { src: '/sponsor/syneps.png', alt: 'Syneps Logo', hint: 'company logo' },
   ];
   const mediaPartnerLogos = [
     { src: '/medpart/BNCC for white bg.png', alt: 'BNCC', hint: 'organization logo' },
